8b4b3bc5c3daaa453b941f6a03c1954839b0efbb
Manrick Leal <55140949+rickleal@users.noreply.github.com>
2021-07-02 12:02:24 -0300

    Update 5 Tabelas Hash.py

--- 5 Tabelas Hash.py
+++ 5 Tabelas Hash.py
@@ -6,13 +6,14 @@ check = 0
 
 while n:
     n -= 1
+    # lista compreensiva ou lambda
     m, c = [int(x) for x in input().split()]
     hash = {str(x) : [] for x in range(m)}
     entrada = [int(x) for x in input().split()]
 
     if check:
         print()
-
+    # sempre pelo resto
     for i in entrada:
         resto = i % m
         hash[str(resto)].append(int(i))
@@ -21,6 +22,7 @@ while n:
         print('%d -> ' % int(i), end = '')
         for j in hash[i]:
             print('%d -> ' % j, end = '')
+        # duas barras para aparecer só uma (funcuntion \n)
         print('\\')
 
     check = 1
