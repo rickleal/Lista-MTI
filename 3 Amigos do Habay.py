096edc680f3dadc595d505b041c746f4d1bb0f7b
Manrick Leal <55140949+rickleal@users.noreply.github.com>
2021-07-02 11:43:46 -0300

    Update 3 Amigos do Habay.py

--- 3 Amigos do Habay.py
+++ 3 Amigos do Habay.py
@@ -1,13 +1,17 @@
+#Rick Leal
+
 inscritos = []
 vencedor = ''
 tam_venc = 0
 
 while True:
+    #simple entrada em lista
     entrada = input().split()
     if entrada[0] == 'FIM':
         break
     inscritos.append(entrada[0])
     tam_nome = len(entrada[0])
+    #trocaa
     if tam_nome > tam_venc:
         vencedor = entrada[0]
         tam_venc = len(vencedor)
